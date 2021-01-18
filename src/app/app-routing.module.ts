66993de93e57baa7fa31a52f509e304c9aa8f44c
Yerkebulan Akhmetzaki <akhmetzaki@gmail.com>
2021-01-18 07:04:50 +0600

    assets + перенос прошлой работы в эту

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogComponent } from './components/blog/blog.component';
+import { MainComponent } from './components/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '**',
+    component: MainComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
